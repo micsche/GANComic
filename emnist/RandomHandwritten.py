681d8d9401ffbd976c41dcaa337ca00e442df2ab
Michael Schembri <31069932+micsche@users.noreply.github.com>
2021-05-16 10:47:38 +0200

    Add files via upload

---
+++ emnist/RandomHandwritten.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+
+
+from tensorflow_examples.models.pix2pix import pix2pix
+
+import tensorflow_datasets as tfds
+
+from IPython.display import clear_output
+import matplotlib.pyplot as plt
+
+dataset, info = tfds.load('oxford_iiit_pet:3.*.*', with_info=True)
+
+
+def normalize(input_image, input_mask):
+  input_image = tf.cast(input_image, tf.float32) / 255.0
+  input_mask -= 1
+  return input_image, input_mask
+
+@tf.function
+def load_image_train(datapoint):
+  input_image = tf.image.resize(datapoint['image'], (128, 128))
+  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+
+  if tf.random.uniform(()) > 0.5:
+    input_image = tf.image.flip_left_right(input_image)
+    input_mask = tf.image.flip_left_right(input_mask)
+
+  input_image, input_mask = normalize(input_image, input_mask)
+
+  return input_image, input_mask
+
+
+def load_image_test(datapoint):
+  input_image = tf.image.resize(datapoint['image'], (128, 128))
+  input_mask = tf.image.resize(datapoint['segmentation_mask'], (128, 128))
+
+  input_image, input_mask = normalize(input_image, input_mask)
+
+  return input_image, input_mask
+
+TRAIN_LENGTH = info.splits['train'].num_examples
+BATCH_SIZE = 64
+BUFFER_SIZE = 1000
+STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
+
+train = dataset['train'].map(load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
+test = dataset['test'].map(load_image_test)
